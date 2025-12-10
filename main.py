927c533d6ced02d19b7e2b920d5c32a385c1dafc
Chetan Nandgave <chetanndkte@gmail.com>
2025-12-11 00:32:12 +0530

    rect area peri

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import arithmetic
+import geometry as geo
+from geometry import calc_rect_peri
+
 print("Hello,World")
+num1=int(input("Enter Num1:"))
+num2=int(input("Enter Num2:"))
+
+arithmetic.add(num1,num2)
+arithmetic.subtract(num1,num2)
+geo.calc_rect_area(num1,num2)
